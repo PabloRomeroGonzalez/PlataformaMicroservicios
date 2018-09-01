e15103edd3e1926df3fc39a99e8c9c1aa0fc8bb6
casa <casa@DESKTOP-UL27R3K>
2018-09-01 21:01:48 +0200

    r

--- EmployeeService/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
+++ EmployeeService/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
@@ -2,7 +2,9 @@ package pl.piomin.services.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import pl.piomin.services.employee.model.Employee;
@@ -16,6 +18,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
 @EnableSwagger2
 public class EmployeeApplication {
 
